d8cd999991ea0f25d2423251c783f76149953193
Eduardo Ramos <eduardo@edmos.com.br>
2020-01-31 13:55:57 -0300

    Atualização

--- poc-camunda-bpm-spring-boot/src/main/java/br/com/edmos/camunda/main/ProcessApplication.java
+++ poc-camunda-bpm-spring-boot/src/main/java/br/com/edmos/camunda/main/ProcessApplication.java
@@ -1,13 +1,16 @@
 package br.com.edmos.camunda.main;
 
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.camunda.bpm.spring.boot.starter.event.PreUndeployEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication("myProcessApplication")
 public class ProcessApplication {
 
 	@Autowired
