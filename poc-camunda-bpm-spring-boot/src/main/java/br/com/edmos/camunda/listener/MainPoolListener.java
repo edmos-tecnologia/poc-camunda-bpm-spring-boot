c2ba4d62129adcf41e53493aea1f4c6f11ada162
Eduardo Ramos <eduardo@edmos.com.br>
2020-02-14 17:02:58 -0300

    Application class

---
+++ poc-camunda-bpm-spring-boot/src/main/java/br/com/edmos/camunda/listener/MainPoolListener.java
@@ -0,0 +1,17 @@
+package br.com.edmos.camunda.listener;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component("mainPoolListener")
+public class MainPoolListener implements ExecutionListener {
+	
+	private static Logger Log = LoggerFactory.getLogger(MainPoolListener.class);
+	
+	public void notify(DelegateExecution execution) throws Exception {
+		Log.info("Entrando no MainPool");
+	}
+}
