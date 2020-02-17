cbccaa4b0515da3efedfa5623cb80e2cb5103ca6
Eduardo Ramos <eduardo@edmos.com.br>
2020-02-17 12:56:25 -0300

    ...

--- poc-camunda-bpm-spring-boot/src/main/java/br/com/edmos/camunda/rest/services/CallbackService.java
+++ poc-camunda-bpm-spring-boot/src/main/java/br/com/edmos/camunda/rest/services/CallbackService.java
@@ -1,13 +1,20 @@
 package br.com.edmos.camunda.rest.services;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CallbackService {
 	
+	@Autowired
+	private RuntimeService runtimeService;
+	
 	@GetMapping("/callbackStart")
 	public String callbackStart() {
+		ProcessInstance pi = runtimeService.startProcessInstanceByKey("CallbackProcess");
 		return "Callback started";
 	}
 }
