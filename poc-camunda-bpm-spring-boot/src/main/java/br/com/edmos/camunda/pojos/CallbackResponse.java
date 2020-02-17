a7c45466ad76968e6e5dc46aafb3aad85d6df94b
Eduardo Ramos <eduardo@edmos.com.br>
2020-02-17 15:01:35 -0300

    ...

---
+++ poc-camunda-bpm-spring-boot/src/main/java/br/com/edmos/camunda/pojos/CallbackResponse.java
@@ -0,0 +1,14 @@
+package br.com.edmos.camunda.pojos;
+
+public class CallbackResponse {
+	
+	private String correlationId;
+
+	public String getCorrelationId() {
+		return correlationId;
+	}
+
+	public void setCorrelationId(String correlationId) {
+		this.correlationId = correlationId;
+	}
+}
--- poc-camunda-bpm-spring-boot/src/main/java/br/com/edmos/camunda/rest/services/CallbackService.java
+++ poc-camunda-bpm-spring-boot/src/main/java/br/com/edmos/camunda/rest/services/CallbackService.java
@@ -1,20 +1,41 @@
 package br.com.edmos.camunda.rest.services;
 
+import java.util.Date;
+
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.edmos.camunda.pojos.CallbackResponse;
 
 @RestController
 public class CallbackService {
 	
+	protected static Logger Log = LoggerFactory.getLogger(CallbackService.class);
+
 	@Autowired
 	private RuntimeService runtimeService;
-	
-	@GetMapping("/callbackStart")
-	public String callbackStart() {
-		ProcessInstance pi = runtimeService.startProcessInstanceByKey("CallbackProcess");
-		return "Callback started";
+
+	@PostMapping("/callbackStart")
+	public CallbackResponse callbackStart(@RequestParam(name = "businessKey")String businessKey) {
+		ProcessInstance pi = runtimeService.startProcessInstanceByKey("CallbackProcess", businessKey);
+		CallbackResponse response = new CallbackResponse();
+		
+		Log.info("BusinessKey: " + pi.getBusinessKey());
+		
+		MessageCorrelationResult result = runtimeService.createMessageCorrelation("callback")
+				.processInstanceBusinessKey(pi.getBusinessKey())
+				.setVariable("callbackStatus", "Callback started")
+				.correlateWithResult();
+		
+		response.setCorrelationId(result.toString());
+		
+		return response;
 	}
 }
