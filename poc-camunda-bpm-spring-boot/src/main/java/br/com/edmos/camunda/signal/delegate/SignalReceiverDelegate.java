773cb607ea98e73a05cf9f36ce50459eae2cd221
Eduardo Ramos <eduardo@edmos.com.br>
2020-02-18 15:21:02 -0300

    Signal Receiver

---
+++ poc-camunda-bpm-spring-boot/src/main/java/br/com/edmos/camunda/signal/delegate/SignalReceiverDelegate.java
@@ -0,0 +1,17 @@
+package br.com.edmos.camunda.signal.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.springframework.stereotype.Component;
+
+import br.com.edmos.camunda.delegate.AbstractDelegate;
+
+@Component("signalReceiverDelegate")
+public class SignalReceiverDelegate extends AbstractDelegate {
+	
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		super.execute(execution);
+		Log.info("###### Signal Received ######");
+	}
+	
+}
